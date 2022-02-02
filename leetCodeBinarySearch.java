b7aa305285b0ad12b14993f4e021e8368ac4d958
KulshresthJangid <71918448+KulshresthJangid@users.noreply.github.com>
2022-02-02 23:08:39 +0530

    Guess the number question

--- leetCodeBinarySearch.java
+++ leetCodeBinarySearch.java
@@ -4,6 +4,9 @@ public class leetCodeBinarySearch {
     public static void main(String[] args) {
         int ans = mySqrt(55);
         System.out.println("------------------------" + ans);
+        int x = guessNumber(10);
+        System.out.println(x);
+
     }
 
     // Solution for the square root problem
@@ -20,5 +23,37 @@ public class leetCodeBinarySearch {
         }
         return l-1;
     }
+// 374. Guess Number Higher or Lower
+    public static int guess(int x) {
+        int pick = 6;
+        if(x>pick) {
+            return -1;
+        } else if (x<pick) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    public static int guessNumber(int n) {
+        int start =1;
+        int end =n;
 
+        while(start<=end)
+        {
+            int mid = start +(end-start)/2;
+            int guess = guess(mid);
+
+            if(guess == 1){
+                start = mid+1;
+            }
+            else if(guess == -1) {
+                end = mid-1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return -1;
+    }
 }
